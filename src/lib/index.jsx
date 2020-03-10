565dbd29bd2008c5d9a30df8521a883bbccb51c8
Inhyuk Na <ina@dblab.postech.ac.kr>
2020-03-10 13:56:02 +0900

    add insert_with_checkbox_table

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -5,99 +5,105 @@ import 'botui/build/botui-theme-default.css';
 
 class Botui extends React.Component {
   message = {
-    add (message) {
-      return this.botui.message.add (message);
+    add(message) {
+      return this.botui.message.add(message);
     },
-    bot (message) {
-      return this.botui.message.bot (message);
+    bot(message) {
+      return this.botui.message.bot(message);
     },
-    insert_with_button (index, message) {
-      return this.botui.message.insert_with_button (index, message);
+    insert_with_button(index, message) {
+      return this.botui.message.insert_with_button(index, message);
     },
-    human (message) {
-      return this.botui.message.human (message);
+    insert_with_checkbox_table(index, message) {
+      return this.botui.message.insert_with_checkbox_table(index, message);
     },
-    get (index) {
-      return this.botui.message.get (index);
+    human(message) {
+      return this.botui.message.human(message);
     },
-    update (index, message) {
-      return this.botui.message.update (index, message);
+    get(index) {
+      return this.botui.message.get(index);
     },
-    insert (index, message) {
-      return this.botui.message.insert (index, message);
+    update(index, message) {
+      return this.botui.message.update(index, message);
     },
-    remove (index) {
-      return this.botui.message.remove (index);
+    insert(index, message) {
+      return this.botui.message.insert(index, message);
     },
-    removeAll () {
-      return this.botui.message.removeAll ();
+    remove(index) {
+      return this.botui.message.remove(index);
     },
-    getMessageLengthCorrectPair () {
-      return this.botui.message.getMessageLengthCorrectPair ();
+    removeAll() {
+      return this.botui.message.removeAll();
     },
-    enableIncorrectClick (callback) {
-      return this.botui.message.enableIncorrectClick (callback);
+    getMessageLengthCorrectPair() {
+      return this.botui.message.getMessageLengthCorrectPair();
     },
-    disableIncorrectClick () {
-      return this.botui.message.disableIncorrectClick ();
+    enableIncorrectClick(callback) {
+      return this.botui.message.enableIncorrectClick(callback);
+    },
+    disableIncorrectClick() {
+      return this.botui.message.disableIncorrectClick();
     },
   };
 
   action = {
-    show (action) {
+    show(action) {
       if (action.type === 'button') {
-        return this.botui.action.button (action);
+        return this.botui.action.button(action);
       }
       if (action.type === 'text') {
-        return this.botui.action.text (action);
+        return this.botui.action.text(action);
       }
-      return this.botui.action.show (action);
+      return this.botui.action.show(action);
     },
-    hide () {
-      return this.botui.action.hide ();
+    hide() {
+      return this.botui.action.hide();
     },
-    text (action) {
-      return this.botui.action.text (action);
+    text(action) {
+      return this.botui.action.text(action);
     },
-    button (action) {
-      return this.botui.action.button (action);
+    button(action) {
+      return this.botui.action.button(action);
     },
-    select (action) {
-      return this.botui.action.select (action);
+    select(action) {
+      return this.botui.action.select(action);
     },
   };
 
-  componentDidMount () {
+  componentDidMount() {
     const BotUI = window.BotUI;
-    this.botui = new BotUI ('react-botui');
-    this.message.add = this.message.add.bind (this);
-    this.message.bot = this.message.bot.bind (this);
-    this.message.insert_with_button = this.message.insert_with_button.bind (
+    this.botui = new BotUI('react-botui');
+    this.message.add = this.message.add.bind(this);
+    this.message.bot = this.message.bot.bind(this);
+    this.message.insert_with_button = this.message.insert_with_button.bind(
       this
     );
-    this.message.human = this.message.human.bind (this);
-    this.message.get = this.message.get.bind (this);
-    this.message.update = this.message.update.bind (this);
-    this.message.remove = this.message.remove.bind (this);
-    this.message.removeAll = this.message.removeAll.bind (this);
-    this.message.insert = this.message.insert.bind (this);
-    this.message.getMessageLengthCorrectPair = this.message.getMessageLengthCorrectPair.bind (
+    this.message.insert_with_checkbox_table = this.message.insert_with_checkbox_table.bind(
       this
     );
-    this.message.enableIncorrectClick = this.message.enableIncorrectClick.bind (
+    this.message.human = this.message.human.bind(this);
+    this.message.get = this.message.get.bind(this);
+    this.message.update = this.message.update.bind(this);
+    this.message.remove = this.message.remove.bind(this);
+    this.message.removeAll = this.message.removeAll.bind(this);
+    this.message.insert = this.message.insert.bind(this);
+    this.message.getMessageLengthCorrectPair = this.message.getMessageLengthCorrectPair.bind(
       this
     );
-    this.message.disableIncorrectClick = this.message.disableIncorrectClick.bind (
+    this.message.enableIncorrectClick = this.message.enableIncorrectClick.bind(
       this
     );
-    this.action.hide = this.action.hide.bind (this);
-    this.action.show = this.action.show.bind (this);
-    this.action.text = this.action.text.bind (this);
-    this.action.button = this.action.button.bind (this);
-    this.action.select = this.action.select.bind (this);
+    this.message.disableIncorrectClick = this.message.disableIncorrectClick.bind(
+      this
+    );
+    this.action.hide = this.action.hide.bind(this);
+    this.action.show = this.action.show.bind(this);
+    this.action.text = this.action.text.bind(this);
+    this.action.button = this.action.button.bind(this);
+    this.action.select = this.action.select.bind(this);
   }
 
-  render () {
+  render() {
     return (
       <div
         className="botui-app-container"
